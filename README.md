38b9ded09b8a0646e21f6a2f7679e80846d90494
FrancesHitchcock <95710313+FrancesHitchcock@users.noreply.github.com>
2022-09-27 10:49:01 +0100

    Commit Initial Files

---
+++ README.md
@@ -0,0 +1,14 @@
+# Unit Converter
+
+## Scrimba Solo Project
+
+This is a Frontend Developer Module 3 JavaScript Challenge. The project was completed from scratch using a Figma design file for the user interface. The project demonstrates the following JavaScript concepts: 
+
+- const variables
+- addEventListener()
+- innerHTML
+- input.value
+- template strings
+- objects in arrays
+
+27 September 2022
---
+++ index.js
@@ -0,0 +1,81 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const valueInput = document.getElementById("value-input")
+const convertBtn = document.getElementById("convert-btn")
+const readoutDiv = document.getElementById("readout-div")
+const uiContainerDiv = document.getElementById("uiContainer-div")
+
+const data = [
+    {
+        title: "Length (Meters/Feet)",
+        imperialUnit: "feet",
+        metricUnit: "meters",
+        imperialSingular: "foot",
+        metricSingular: "meter",
+        multiplier: 3.281
+    },
+    {
+        title: "Volume (Liters/Gallons)",
+        imperialUnit: "gallons",
+        metricUnit: "liters",
+        imperialSingular: "gallon",
+        metricSingular: "liter",
+        multiplier: 0.264
+    },
+    {
+        title: "Mass (Kilograms/Pounds)",
+        imperialUnit: "pounds",
+        metricUnit: "kilograms",
+        imperialSingular: "pound",
+        metricSingular: "kilogram",
+        multiplier: 2.204
+    }
+]
+
+valueInput.value = 20
+
+convert()
+
+valueInput.addEventListener("focus", function() {
+    valueInput.value = ""
+})
+
+convertBtn.addEventListener("click", function() {
+    convert()
+})
+
+function convert() {
+    const userInput = valueInput.value
+
+    if (isNaN(userInput)) {
+        return
+    }
+
+    let readoutDivDOM = ""
+
+    for (let i = 0; i < data.length; i++) {
+
+        const imperialValue = (userInput * data[i].multiplier).toFixed(3)
+        const metericValue = (userInput / data[i].multiplier).toFixed(3)
+
+        let metricString = data[i].metricUnit
+        let imperialString = data[i].imperialUnit
+
+        if (Number(userInput) === 1) {
+            metricString = data[i].metricSingular
+            imperialString = data[i].imperialSingular
+        }
+
+        readoutDivDOM += `
+            <div class="unit-div">
+                <h2>${data[i].title}</h2>
+                <h3>${userInput} ${metricString} = ${imperialValue} ${data[i].imperialUnit} | ${userInput} ${imperialString} = ${metericValue} ${data[i].metricUnit}</h3>
+            </div>
+        `
+    }
+    readoutDiv.innerHTML = readoutDivDOM
+}
